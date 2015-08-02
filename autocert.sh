b15ebef380b88a4e214ceb77e55615a7355577b4
ainominako <ainominako@users.noreply.github.com>
2015-08-02 19:02:07 +0900

    Update autocert.sh

--- autocert.sh
+++ autocert.sh
@@ -1,22 +1,8 @@
 #! /bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-#===============================================================================================
-#   System Required:  CentOS6.x (32bit/64bit) or Ubuntu
-#   Description:  Install IKEV2 VPN for CentOS and Ubuntu
-#   Author: quericy
-#   Intro:  http://quericy.me/blog/699
-#===============================================================================================
-
 clear
-echo "#############################################################"
-echo "# Install IKEV2 VPN for CentOS6.x (32bit/64bit) or Ubuntu"
-echo "# Intro: http://quericy.me/blog/699"
-echo "#"
-echo "# Author:quericy"
-echo "#"
-echo "#############################################################"
-echo ""
+
 
 # Install IKEV2
 function install_ikev2(){
@@ -24,15 +10,8 @@ function install_ikev2(){
 	disable_selinux
 	get_my_ip
 	get_system
-	yum_install
 	pre_install
-	download_files
-	setup_strongswan
 	get_key
-	configure_ipsec
-	configure_strongswan
-	configure_secrets
-	iptables_set
 	ipsec start
 	success_info
 }
@@ -85,49 +64,16 @@ function get_system(){
 
 # Pre-installation settings
 function pre_install(){
-	echo "#############################################################"
-	echo "# Install IKEV2 VPN for CentOS6.x (32bit/64bit) or Ubuntu"
-	echo "# Intro: http://quericy.me/blog/699"
-	echo "#"
-	echo "# Author:quericy"
-	echo "#"
-	echo "#############################################################"
-	echo ""
-    echo "please choose the type of your VPS(Xen、KVM: 1  ,  OpenVZ: 2):"
-    read -p "your choice(1 or 2):" os_choice
-    if [ "$os_choice" = "1" ]; then
-        os="1"
-		os_str="Xen、KVM"
-		else
-			if [ "$os_choice" = "2" ]; then
-				os="2"
-				os_str="OpenVZ"
-				else
-				echo "wrong choice!"
-				exit 1
-			fi
-    fi
-	echo "please input the ip (or domain) of your VPS:"
+　　os="1"
+　　os_str="Xen、KVM"
+　　echo "please input the ip (or domain) of your VPS:"
     read -p "ip or domain(default_vale:${IP}):" vps_ip
 	if [ "$vps_ip" = "" ]; then
 		vps_ip=$IP
 	fi
-	echo "please input the cert country(C):"
-    read -p "C(default value:com):" my_cert_c
-	if [ "$my_cert_c" = "" ]; then
-		my_cert_c="com"
-	fi
-	echo "please input the cert organization(O):"
-    read -p "O(default value:myvpn):" my_cert_o
-	if [ "$my_cert_o" = "" ]; then
-		my_cert_o="myvpn"
-	fi
-	echo "please input the cert common name(CN):"
-    read -p "CN(default value:VPN CA):" my_cert_cn
-	if [ "$my_cert_cn" = "" ]; then
-		my_cert_cn="VPN CA"
-	fi
-	echo "####################################"
+　　my_cert_c="com"
+　　my_cert_o="myvpn"
+　　my_cert_cn="VPN CA"
     get_char(){
         SAVEDSTTY=`stty -g`
         stty -echo
@@ -148,57 +94,6 @@ function pre_install(){
 	#Current folder
     cur_dir=`pwd`
     cd $cur_dir
-}
-
-#install necessary lib
-function yum_install(){
-	if [ "$system_str" = "0" ]; then
-	yum -y update
-	yum -y install pam-devel openssl-devel make gcc
-	else
-	apt-get -y update
-	apt-get -y install libpam0g-dev libssl-dev make gcc
-	fi
-}
-
-# Download strongswan
-function download_files(){
-    if [ -f strongswan.tar.gz ];then
-        echo -e "strongswan.tar.gz [\033[32;1mfound\033[0m]"
-    else
-        if ! wget http://download.strongswan.org/strongswan.tar.gz;then
-            echo "Failed to download strongswan.tar.gz"
-            exit 1
-        fi
-    fi
-    tar xzf strongswan.tar.gz
-    if [ $? -eq 0 ];then
-        cd $cur_dir/strongswan-*/
-    else
-        echo ""
-        echo "Unzip strongswan.tar.gz failed! Please visit http://quericy.me/blog/699 and contact."
-        exit 1
-    fi
-}
-
-# configure and install strongswan
-function setup_strongswan(){
-	if [ "$os" = "1" ]; then
-		./configure  --enable-eap-identity --enable-eap-md5 \
---enable-eap-mschapv2 --enable-eap-tls --enable-eap-ttls --enable-eap-peap  \
---enable-eap-tnc --enable-eap-dynamic --enable-eap-radius --enable-xauth-eap  \
---enable-xauth-pam  --enable-dhcp  --enable-openssl  --enable-addrblock --enable-unity  \
---enable-certexpire --enable-radattr --enable-tools --enable-openssl --disable-gmp
-
-	else
-		./configure  --enable-eap-identity --enable-eap-md5 \
---enable-eap-mschapv2 --enable-eap-tls --enable-eap-ttls --enable-eap-peap  \
---enable-eap-tnc --enable-eap-dynamic --enable-eap-radius --enable-xauth-eap  \
---enable-xauth-pam  --enable-dhcp  --enable-openssl  --enable-addrblock --enable-unity  \
---enable-certexpire --enable-radattr --enable-tools --enable-openssl --disable-gmp --enable-kernel-libipsec
-
-	fi
-	make; make install
 }
 
 # configure cert and key
@@ -253,149 +148,10 @@ function get_key(){
 	
 }
 
-# configure the ipsec.conf
-function configure_ipsec(){
- cat > /usr/local/etc/ipsec.conf<<-EOF
-config setup
-    uniqueids=never 
-conn iOS_cert
-    keyexchange=ikev1
-    fragmentation=yes
-    left=%defaultroute
-    leftauth=pubkey
-    leftsubnet=0.0.0.0/0
-    leftcert=server.cert.pem
-    right=%any
-    rightauth=pubkey
-    rightauth2=xauth
-    rightsourceip=10.31.2.0/24
-    rightcert=client.cert.pem
-    auto=add
-conn android_xauth_psk
-    keyexchange=ikev1
-    left=%defaultroute
-    leftauth=psk
-    leftsubnet=0.0.0.0/0
-    right=%any
-    rightauth=psk
-    rightauth2=xauth
-    rightsourceip=10.31.2.0/24
-    auto=add
-conn networkmanager-strongswan
-    keyexchange=ikev2
-    left=%defaultroute
-    leftauth=pubkey
-    leftsubnet=0.0.0.0/0
-    leftcert=server.cert.pem
-    right=%any
-    rightauth=pubkey
-    rightsourceip=10.31.2.0/24
-    rightcert=client.cert.pem
-    auto=add
-conn windows7
-    keyexchange=ikev2
-    ike=aes256-sha1-modp1024!
-    rekey=no
-    left=%defaultroute
-    leftauth=pubkey
-    leftsubnet=0.0.0.0/0
-    leftcert=server.cert.pem
-    right=%any
-    rightauth=eap-mschapv2
-    rightsourceip=10.31.2.0/24
-    rightsendcert=never
-    eap_identity=%any
-    auto=add
-EOF
-}
-
-# configure the strongswan.conf
-function configure_strongswan(){
- cat > /usr/local/etc/strongswan.conf<<-EOF
- charon {
-        load_modular = yes
-        duplicheck.enable = no
-        compress = yes
-        plugins {
-                include strongswan.d/charon/*.conf
-        }
-        dns1 = 8.8.8.8
-        dns2 = 8.8.4.4
-        nbns1 = 8.8.8.8
-        nbns2 = 8.8.4.4
-}
-include strongswan.d/*.conf
-EOF
-}
-
-# configure the ipsec.secrets
-function configure_secrets(){
-	cat > /usr/local/etc/ipsec.secrets<<-EOF
-: RSA server.pem
-: PSK "myPSKkey"
-: XAUTH "myXAUTHPass"
-myUserName %any : EAP "myUserPass"
-	EOF
-}
-
-# iptables set
-function iptables_set(){
-    sysctl -w net.ipv4.ip_forward=1
-    if [ "$os" = "1" ]; then
-		iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-		iptables -A FORWARD -s 10.31.0.0/24  -j ACCEPT
-		iptables -A FORWARD -s 10.31.1.0/24  -j ACCEPT
-		iptables -A FORWARD -s 10.31.2.0/24  -j ACCEPT
-		iptables -A INPUT -i eth0 -p esp -j ACCEPT
-		iptables -A INPUT -i eth0 -p udp --dport 500 -j ACCEPT
-		iptables -A INPUT -i eth0 -p tcp --dport 500 -j ACCEPT
-		iptables -A INPUT -i eth0 -p udp --dport 4500 -j ACCEPT
-		iptables -A INPUT -i eth0 -p udp --dport 1701 -j ACCEPT
-		iptables -A INPUT -i eth0 -p tcp --dport 1723 -j ACCEPT
-		iptables -A FORWARD -j REJECT
-		iptables -t nat -A POSTROUTING -s 10.31.0.0/24 -o eth0 -j MASQUERADE
-		iptables -t nat -A POSTROUTING -s 10.31.1.0/24 -o eth0 -j MASQUERADE
-		iptables -t nat -A POSTROUTING -s 10.31.2.0/24 -o eth0 -j MASQUERADE
-	else
-		iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-		iptables -A FORWARD -s 10.31.0.0/24  -j ACCEPT
-		iptables -A FORWARD -s 10.31.1.0/24  -j ACCEPT
-		iptables -A FORWARD -s 10.31.2.0/24  -j ACCEPT
-		iptables -A INPUT -i venet0 -p esp -j ACCEPT
-		iptables -A INPUT -i venet0 -p udp --dport 500 -j ACCEPT
-		iptables -A INPUT -i venet0 -p tcp --dport 500 -j ACCEPT
-		iptables -A INPUT -i venet0 -p udp --dport 4500 -j ACCEPT
-		iptables -A INPUT -i venet0 -p udp --dport 1701 -j ACCEPT
-		iptables -A INPUT -i venet0 -p tcp --dport 1723 -j ACCEPT
-		iptables -A FORWARD -j REJECT
-		iptables -t nat -A POSTROUTING -s 10.31.0.0/24 -o venet0 -j MASQUERADE
-		iptables -t nat -A POSTROUTING -s 10.31.1.0/24 -o venet0 -j MASQUERADE
-		iptables -t nat -A POSTROUTING -s 10.31.2.0/24 -o venet0 -j MASQUERADE
-    fi
-	if [ "$system_str" = "0" ]; then
-		service iptables save
-	else
-		iptables-save > /etc/iptables.rules
-		cat > /etc/network/if-up.d/iptables<<EOF
-#!/bin/sh
-iptables-restore < /etc/iptables.rules
-EOF
-		chmod +x /etc/network/if-up.d/iptables
-	fi
-}
-
 # echo the success info
 function success_info(){
 	echo "#############################################################"
-	echo -e "#"
-	echo -e "# [\033[32;1mInstall Successful\033[0m]"
-	echo -e "# There is the default login info of your VPN"
-	echo -e "# UserName:\033[33;1m myUserName\033[0m"
-	echo -e "# PassWord:\033[33;1m myUserPass\033[0m"
-	echo -e "# PSK:\033[33;1m myPSKkey\033[0m"
-	echo -e "# you can change UserName and PassWord in\033[32;1m /usr/local/etc/ipsec.secrets\033[0m"
-	echo -e "# you must copy the cert \033[32;1m ${cur_dir}/my_key/ca.cert.pem \033[0m to the client and install it."
-	echo -e "#"
+	echo -e "DONE"
 	echo -e "#############################################################"
 	echo -e ""
 }
